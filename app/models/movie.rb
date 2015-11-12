3fb9353733dc105add3c40a70c7db2016e57dd5b
Ponser <kenlathan@me.com>
2015-11-12 19:22:58 +0000

    movie.rb amended to have only G, PG, PG-13, and R

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,6 @@ class Movie < ActiveRecord::Base
   # https://github.com/saasbook/hw-rails-intro#part-2-filter-the-list-of-movies-by-rating-15-points
   #
   def self.all_ratings
-    return ['G', 'PG', 'PG-13', 'R', 'NC-17']
+    return ['G', 'PG', 'PG-13', 'R'] # 'NC-17' and 'X' left out on purpose
   end
 end
