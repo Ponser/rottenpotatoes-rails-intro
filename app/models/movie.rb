6baf7a57ebad22ae22fa17feadb43eba5f4d2929
Ponser <kenlathan@me.com>
2015-11-12 14:51:14 +0000

    movie.rb add class method all_ratings

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  
+  ################################################################################
+  # https://github.com/saasbook/hw-rails-intro#part-2-filter-the-list-of-movies-by-rating-15-points
+  #
+  def self.all_ratings
+    return ['G', 'PG', 'PG-13', 'R', 'NC-17']
+  end
 end
